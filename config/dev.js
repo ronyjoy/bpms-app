c67523aab9508ab9adcc60d5a04bbf7361fbe6af
Rony <ronyjoy@gmail.com>
2018-12-16 21:04:44 -0600

    Create dev.js

---
+++ config/dev.js
@@ -0,0 +1,7 @@
+//dev.js donot commit this
+module.exports = {
+    googleClientID:'589283510408-kt4ejvc7lcpdpomdumi0f05r9e2kr8kj.apps.googleusercontent.com',
+    googleClientSecret:'yZ5v7ugeWqai35Zh5TOuN4GM',
+    mongoURI : 'mongodb://bpms:Tenergy_80@ds241578.mlab.com:41578/bpms',
+    cookieKey : 'ouhoneuho890g23ypsnttnbdxhtcndrlgaodeucroheunho-lieljbxsaehduigrlasoeuhsnthosehud'
+}
