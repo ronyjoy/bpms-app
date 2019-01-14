96db76387acaf4e2e7fd738cba54a39b07ea6fb8
Rony Joy <Rony.Joy@gemalto.com>
2019-01-14 03:21:33 -0600

    add customer

---
+++ client/src/views/Customer/AddCustomer.js
@@ -0,0 +1,121 @@
+import React from 'react';
+import { Formik, Field, Form, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+import {
+	Container,
+	Badge,
+	Button,
+	ButtonDropdown,
+	Card,
+	CardBody,
+	CardFooter,
+	CardHeader,
+	Col,
+	Collapse,
+	DropdownItem,
+	DropdownMenu,
+	DropdownToggle,
+	Fade,
+	FormGroup,
+	FormText,
+	FormFeedback,
+	Input,
+	InputGroup,
+	InputGroupAddon,
+	InputGroupText,
+	Label,
+	Row,
+} from 'reactstrap';
+import { ReactstrapInput } from "reactstrap-formik";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+// Async Validation
+
+const validationSchema = Yup.object().shape({
+	email: Yup.string()
+	  .email('E-mail is not valid!')
+	  .required('E-mail is required!'),
+	address: Yup.string()
+	  .min(6, 'Address has to be longer than 6 characters!')  
+	  .required('Address is required!'),
+	customername: Yup.string()
+	  .required('Customer Name is required!'),
+	contactperson: Yup.string()
+	  .required('Contact Person Name is required!'),
+	phone: Yup.number() 
+	  .required('phone is required!'),
+  })
+
+
+const AddCustomer = () => (
+
+
+
+	<Formik
+		initialValues={{ customername: "", contactperson: "",address:"", email:"", phone:"" }}
+		validationSchema={validationSchema}
+		onSubmit={(values, { setSubmitting }) => {
+			console.log(values);
+			//Make API calls here
+			axios.post("/api/customer", JSON.stringify(values, null, 2));
+		}}
+		render={({ submitForm, isSubmitting, values }) => (
+			<Form>
+				<Container >
+					<Row>
+
+
+
+						<Col md="12" sm="6">
+							<Card>
+								<CardHeader>
+									<strong>New</strong>
+									<small> Customer</small>
+								</CardHeader>
+								<CardBody>
+									<FormGroup>
+										<FormGroup row className="my-0">
+											<Col xs="8">
+												<FormGroup>
+													<Field type="text" label="Customer Name" name="customername" placeholder="ABC Customer" component={ReactstrapInput} />
+												</FormGroup>
+											</Col>
+											<Col xs="4">
+												<FormGroup>
+													<Field type="text" label="Contact Person" name="contactperson" placeholder="Contact Person" component={ReactstrapInput} />
+												</FormGroup>
+											</Col>
+										</FormGroup>
+										<FormGroup>
+											<Field type="text" label="Address" name="address" placeholder="Address" component={ReactstrapInput} />
+
+										</FormGroup>
+										<FormGroup row className="my-0">
+											<Col xs="8">
+												<FormGroup>
+													<Field type="email" label="Email" name="email" component={ReactstrapInput} />
+												</FormGroup>
+											</Col>
+											<Col xs="4">
+												<FormGroup>
+													<Field type="text" label="Phone" name="phone" component={ReactstrapInput} />
+												</FormGroup>
+											</Col>
+										</FormGroup>
+									</FormGroup>
+								</CardBody>
+								<CardFooter>
+									<Button type="submit" size="sm" color="primary"><i className="fa fa-dot-circle-o"></i> Submit</Button>
+									<Button type="reset" size="sm" color="danger"><i className="fa fa-ban"></i> Reset</Button>
+								</CardFooter>
+							</Card>
+						</Col>
+
+					</Row>
+				</Container>
+			</Form >
+		)}
+	/>
+);
+
+export default AddCustomer;
