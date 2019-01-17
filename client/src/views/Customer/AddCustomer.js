3af0c2d82364900239bb07169e054f15d9dee4be
Rony Joy <Rony.Joy@gemalto.com>
2019-01-16 21:09:34 -0600

    changing the ui of add customer

--- client/src/views/Customer/AddCustomer.js
+++ client/src/views/Customer/AddCustomer.js
@@ -1,134 +1,104 @@
 import React from 'react';
 import { Formik, Field, Form, ErrorMessage } from 'formik';
-import * as Yup from 'yup';
 import axios from 'axios';
-import {
-	Container,
-	Badge,
-	Button,
-	ButtonDropdown,
-	Card,
-	CardBody,
-	CardFooter,
-	CardHeader,
-	Col,
-	Collapse,
-	DropdownItem,
-	DropdownMenu,
-	DropdownToggle,
-	Fade,
-	FormGroup,
-	FormText,
-	FormFeedback,
-	Input,
-	InputGroup,
-	InputGroupAddon,
-	InputGroupText,
-	Label,
-	Row,
-} from 'reactstrap';
-import { ReactstrapInput } from "reactstrap-formik";
-import "bootstrap/dist/css/bootstrap.min.css";
+import Button from '@material-ui/core/Button';
+import { LinearProgress, MenuItem } from '@material-ui/core';
+import * as Yup from 'yup'
 import isEmpty from 'lodash/isEmpty';
+import classNames from 'classnames';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import IconButton from '@material-ui/core/IconButton';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import Visibility from '@material-ui/icons/Visibility';
+import VisibilityOff from '@material-ui/icons/VisibilityOff';
+import Badge from '@material-ui/core/Badge';
+
+import {
+	fieldToTextField,
+	TextField,
+	TextFieldProps,
+	Select,
+} from 'formik-material-ui';
+
+
+const styles = theme => ({
+	root: {
+		display: 'flex',
+		flexWrap: 'wrap',
+	},
+	margin: {
+		margin: theme.spacing.unit,
+	},
+	textField: {
+		flexBasis: 200,
+	},
+	button: {
+    margin: theme.spacing.unit,
+  },
+  input: {
+    display: 'none',
+  },
+});
+
 
 // Async Validation
+class AddCustomer extends React.Component {
 
-const validationSchema = Yup.object().shape({
-	email: Yup.string()
-	  .email('E-mail is not valid!')
-	  .required('E-mail is required!'),
-	address: Yup.string()
-	  .min(6, 'Address has to be longer than 6 characters!')  
-	  .required('Address is required!'),
-	customername: Yup.string()
-	  .required('Customer Name is required!'),
-	contactperson: Yup.string()
-	  .required('Contact Person Name is required!'),
-	phone: Yup.number() 
-	  .required('phone is required!'),
-  })
-
-
-const AddCustomer = () => (
+	validationSchema = Yup.object().shape({
+		email: Yup.string()
+			.email('E-mail is not valid!')
+			.required('E-mail is required!'),
+		address: Yup.string()
+			.min(6, 'Address has to be longer than 6 characters!')
+			.required('Address is required!'),
+		customername: Yup.string()
+			.required('Customer Name is required!'),
+		contactperson: Yup.string()
+			.required('Contact Person Name is required!'),
+		phone: Yup.number()
+			.required('phone is required!'),
+	})
 
 
+	render() {
+		const { classes } = this.props;
+		return (
 
-	<Formik
-		initialValues={{ customername: "", contactperson: "",address:"", email:"", phone:"" }}
-		validationSchema={validationSchema}
-		onSubmit={(values, { setSubmitting }) => {
-			console.log(values);
-			//Make API calls here
-			axios.post("/api/customer", values)
-			.then(function (response) {
-				// handle success
-				console.log(response);
-			  })
-			  .catch(function (error) {
-				// handle error
-				console.log(error);
-			  })
-			  .then(function () {
-				// always executed
-			  });
-		}}
-		render={({ values, touched, errors, dirty, isSubmitting , submitForm }) => (
-			<Form>
-				<Container >
-					<Row>
+			<Formik
+				initialValues={{ customername: "", contactperson: "", address: "", email: "", phone: "" }}
+				validationSchema={this.validationSchema}
+				onSubmit={(values, { setSubmitting }) => {
+					console.log(values);
+					//Make API calls here
+					axios.post("/api/customer", values)
+						.then(function (response) {
+							// handle success
+							console.log(response);
+						})
+						.catch(function (error) {
+							// handle error
+							console.log(error);
+						})
+						.then(function () {
+							// always executed
+						});
+				}}
+				render={({ values, touched, errors, dirty, isSubmitting, submitForm }) => (
+					<Form>
+						<Field type="text" variant="outlined" className={classNames(classes.margin, classes.textField)} label="Customer Name" name="customername" placeholder="ABC Customer" component={TextField} />
+						<Field type="text" variant="outlined" className={classNames(classes.margin, classes.textField)} label="Contact Person" name="contactperson" placeholder="Contact Person" component={TextField} />
+						<Field type="text" variant="outlined" label="Address" className={classNames(classes.margin, classes.textField)} name="address" placeholder="Address" component={TextField} />
+						<Field type="email" variant="outlined" label="Email" className={classNames(classes.margin, classes.textField)} name="email" placeholder="email@email.com" component={TextField} />
+						<Field type="text" variant="outlined" label="Phone" className={classNames(classes.margin, classes.textField)} name="phone" placeholder="1111111111" component={TextField} />
+							<Button  className={classes.button} type="submit" variant="contained" color="primary" disabled={isSubmitting || !isEmpty(errors) || !dirty} size="sm" color="primary"> Submit</Button>
+							<Button  className={classes.button} type="reset" variant="contained" color="primary" size="sm" color="danger">Reset</Button>
+					</Form >
+				)}
+			/>
+		);
 
+	}
+}
 
-
-						<Col md="12" sm="6">
-							<Card>
-								<CardHeader>
-									<strong>New</strong>
-									<small> Customer</small>
-								</CardHeader>
-								<CardBody>
-									<FormGroup>
-										<FormGroup row className="my-0">
-											<Col xs="8">
-												<FormGroup>
-													<Field type="text" label="Customer Name" name="customername" placeholder="ABC Customer" component={ReactstrapInput} />
-												</FormGroup>
-											</Col>
-											<Col xs="4">
-												<FormGroup>
-													<Field type="text" label="Contact Person" name="contactperson" placeholder="Contact Person" component={ReactstrapInput} />
-												</FormGroup>
-											</Col>
-										</FormGroup>
-										<FormGroup>
-											<Field type="text" label="Address" name="address" placeholder="Address" component={ReactstrapInput} />
-
-										</FormGroup>
-										<FormGroup row className="my-0">
-											<Col xs="8">
-												<FormGroup>
-													<Field type="email" label="Email" name="email" placeholder="email@email.com"component={ReactstrapInput} />
-												</FormGroup>
-											</Col>
-											<Col xs="4">
-												<FormGroup>
-													<Field type="text" label="Phone" name="phone" placeholder="1111111111" component={ReactstrapInput} />
-												</FormGroup>
-											</Col>
-										</FormGroup>
-									</FormGroup>
-								</CardBody>
-								<CardFooter>
-									<Button type="submit" disabled={isSubmitting || !isEmpty(errors) || !dirty} size="sm" color="primary"><i className="fa fa-dot-circle-o"></i> Submit</Button>
-									<Button type="reset" size="sm" color="danger"><i className="fa fa-ban"></i> Reset</Button>
-								</CardFooter>
-							</Card>
-						</Col>
-
-					</Row>
-				</Container>
-			</Form >
-		)}
-	/>
-);
-
-export default AddCustomer;
+export default withStyles(styles)(AddCustomer);
