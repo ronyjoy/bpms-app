4a7a56d7b33ea913f43067ae26a348c7f3a9467d
Rony Joy <Rony.Joy@gemalto.com>
2019-02-24 01:13:58 -0600

    merging

--- client/src/views/Customer/AddCustomer.js
+++ client/src/views/Customer/AddCustomer.js
@@ -13,58 +13,7 @@ import {addCustomer} from '../../actions/customerActions'
 import {
 	TextField,
 } from 'formik-material-ui';
-<<<<<<< HEAD
-import Autosuggest from 'react-autosuggest';
-import theme from '../../assets/css/autocomplete.css';
-
-// Imagine you have a list of languages that you'd like to autosuggest.
-const languages = [
-	{
-		name: 'C',
-		year: 1972
-	},
-	{
-		name: 'Elm',
-		year: 2012
-	},
-	{
-		name: 'C++',
-		year: 2012
-	},
-	{
-		name: 'C#',
-		year: 2012
-	},
-
-];
-
-// Teach Autosuggest how to calculate suggestions for any given input value.
-const getSuggestions = value => {
-	const inputValue = value.trim().toLowerCase();
-	const inputLength = inputValue.length;
-
-	return inputLength === 0 ? [] : languages.filter(lang =>
-		lang.name.toLowerCase().slice(0, inputLength) === inputValue
-	);
-};
-
-// When suggestion is clicked, Autosuggest needs to populate the input
-// based on the clicked suggestion. Teach Autosuggest how to calculate the
-// input value for every given suggestion.
-const getSuggestionValue = suggestion => suggestion.name;
-
-// Use your imagination to render suggestions.
-const renderSuggestion = suggestion => (
-	<div>
-		{suggestion.name}
-	</div>
-);
-
-
-
-=======
  
->>>>>>> development
 
 const styles = theme => ({
 	root: {
@@ -89,47 +38,9 @@ const styles = theme => ({
 // Async Validation
 class AddCustomer extends React.Component {
 
-<<<<<<< HEAD
-	constructor() {
-		super();
-
-		// Autosuggest is a controlled component.
-		// This means that you need to provide an input value
-		// and an onChange handler that updates this value (see below).
-		// Suggestions also need to be provided to the Autosuggest,
-		// and they are initially empty because the Autosuggest is closed.
-		this.state = {
-			value: '',
-			suggestions: []
-		};
-	}
-
-	onChange = (event, { newValue }) => {
-		this.setState({
-			value: newValue
-		});
-	};
-
-	// Autosuggest will call this function every time you need to update suggestions.
-	// You already implemented this logic above, so just use it.
-	onSuggestionsFetchRequested = ({ value }) => {
-		this.setState({
-			suggestions: getSuggestions(value)
-		});
-	};
-
-	// Autosuggest will call this function every time you need to clear suggestions.
-	onSuggestionsClearRequested = () => {
-		this.setState({
-			suggestions: []
-		});
-	};
-
-=======
 	addCustomer = async (data) => {
     this.props.dispatch(addCustomer(data));
   }
->>>>>>> development
 
 
 	validationSchema = Yup.object().shape({
@@ -150,18 +61,6 @@ class AddCustomer extends React.Component {
 
 	render() {
 
-<<<<<<< HEAD
-		const { value, suggestions } = this.state;
-
-		// Autosuggest will pass through all these props to the input.
-		const inputProps = {
-			placeholder: 'Type a programming language',
-			value,
-			onChange: this.onChange
-		};
-
-=======
->>>>>>> development
 		const { classes } = this.props;
 		return (
 
@@ -186,12 +85,7 @@ class AddCustomer extends React.Component {
 								position: 'top-right',
 								effect: 'scale',
 								offset: 80
-<<<<<<< HEAD
-							});
-						});
-=======
 							  });
->>>>>>> development
 				}}
 				render={x => (
 					<Form>
